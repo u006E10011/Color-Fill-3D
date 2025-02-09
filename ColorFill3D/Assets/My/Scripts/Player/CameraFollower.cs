02581b28bafba0417a2fda3a8fa6304d33daed50
n19 <162562221+u006E10011@users.noreply.github.com>
2025-02-09 20:16:21 +0800

    Configured camera
    Настроил ортографическую камеру

--- ColorFill3D/Assets/My/Scripts/Player/CameraFollower.cs
+++ ColorFill3D/Assets/My/Scripts/Player/CameraFollower.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Project;
 using UnityEngine;
 
 namespace Project
@@ -13,10 +12,13 @@ namespace Project
 
         [SerializeField, Space(10)] private float _smoothing = 3;
 
-        [SerializeField] private float _minValueToCalculateRatio = 8;
-        [SerializeField] private float _minFieldOfView = 50f;
-        [SerializeField] private float _ratioFieldOfView = 3.823529f;
+        [SerializeField] private float _minOrthographicSize = 5.35f;
+        [SerializeField] private float _ratioSize = .25f;
+        [SerializeField] private float _minCountCubeLineToCalculateSize = 8;
+
         [SerializeField] private Camera _camera;
+
+        private float _currentCountCubeLine;
 
         private void OnEnable() => EventBus.Instance.OnMoveCamera += MoveToTarget;
         private void OnDisable() => EventBus.Instance.OnMoveCamera -= MoveToTarget;
@@ -25,9 +27,8 @@ namespace Project
 
         private IEnumerator Follow(Level level)
         {
-            var size = CalculateSize(level, out var sortX, out var sortZ);
-            var position = TargetPosition(size) + Vector3.forward * _offsetY;
-            CalculateFiledOfView(sortX, sortZ);
+            var position = CalculatePosition(level, out var sortX, out var sortZ) + Vector3.forward * _offsetY;
+            CameraSize(sortX, sortZ);
 
             while (Vector3.Distance(transform.position, position) > _offset)
             {
@@ -37,37 +38,37 @@ namespace Project
             }
         }
 
-        private Vector3 CalculateSize(Level level, out List<Transform> sortX, out List<Transform> sortZ)
+        private Vector3 CalculatePosition(Level level, out List<Transform> sortX, out List<Transform> sortZ)
         {
             List<Transform> item = level.gameObject.GetComponentsInChildren<Transform>().ToList();
 
             sortX = item.OrderBy(p => p.transform.position.x).ToList();
             sortZ = item.OrderBy(p => p.transform.position.z).ToList();
 
+            var x = sortX[0].position.x + sortX[^1].position.x;
+            var z = sortZ[0].position.z + sortZ[^1].position.z;
+
             return new()
             {
-                x = sortX[0].position.x + sortX[^1].position.x,
-                y = 0f,
-                z = sortZ[0].position.z + sortZ[^1].position.z
+                x = x / 2,
+                y = transform.position.y,
+                z = z / 2,
             };
         }
 
-        private Vector3 TargetPosition(Vector3 size) => new()
-        {
-            x = size.x / 2,
-            y = transform.position.y,
-            z = size.z / 2,
-        };
-
-        private void CalculateFiledOfView(List<Transform> sortX, List<Transform> sortZ)
+        private void CameraSize(List<Transform> sortX, List<Transform> sortZ)
         {
             var x = Mathf.Abs(sortX[0].localPosition.x) + sortX[^1].localPosition.x;
             var z = Mathf.Abs(sortZ[0].localPosition.z) + sortZ[^1].localPosition.z;
 
-            var count = System.Math.Max(x, z);
-            var value = count > _minValueToCalculateRatio ? _minFieldOfView + ((count  - _minValueToCalculateRatio) * _ratioFieldOfView) : _camera.fieldOfView;
-            Debug.Log($"Count: {count} || Value: {value}");
-            _camera.fieldOfView = Mathf.Clamp(value, _minFieldOfView, int.MaxValue);
+            _currentCountCubeLine = System.Math.Max(x, z);
+
+            float value = 0;
+
+            if (_currentCountCubeLine > _minCountCubeLineToCalculateSize)
+                value = _ratioSize * (_currentCountCubeLine - _minCountCubeLineToCalculateSize);
+
+            _camera.orthographicSize = Mathf.Clamp(_minOrthographicSize + value, _minOrthographicSize, int.MaxValue);
         }
     }
 }
